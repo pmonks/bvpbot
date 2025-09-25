c545e30a8db87b957e26c1321c91f63543d33f33
Peter Monks <pmonks@gmail.com>
2025-09-24 18:55:08 -0700

    :new: Introduce minimum IST title lengths to avoid one-word titles

--- src/bvpbot/source/ist.clj
+++ src/bvpbot/source/ist.clj
@@ -14,21 +14,36 @@
             [clojure.edn        :as edn]
             [markov-chains.core :as mc]))
 
-(def markov-chain (if-let [ist-markov-chain (io/resource "ist-markov-chain.edn")]
-                    (edn/read-string (slurp ist-markov-chain))
-                    (throw (RuntimeException. "ist-markov-chain classpath resource not found - did you remember to run the 'gen-ist-markov' alias first?"))))
+(def ^:private markov-chain (if-let [ist-markov-chain (io/resource "ist-markov-chain.edn")]
+                              (edn/read-string (slurp ist-markov-chain))
+                              (throw (RuntimeException. "ist-markov-chain classpath resource not found - did you remember to run the 'gen-ist-markov' alias first?"))))
+
+(def ^:private min-title-length 2)
+(def ^:private max-title-length 100)
+
+(defn- gen-title-words
+  "Generates no more than `max-title-length` words from the given chain."
+  [chain]
+  (take max-title-length   ; Make sure we eventually drop out
+        (take-while (partial not= "🔚")
+                    (drop-while #(or (= "🔚" %) (re-matches #"(\p{Punct})+" %))   ; Drop leading title breaks and punctuation
+                                (mc/generate chain)))))
+
+(defn- gen-minimum-length-title
+  "Generates between `min-title-length` and `max-title-length` words from the
+  given chain."
+  [chain]
+  (loop [words (gen-title-words chain)]
+    (if (> (count words) min-title-length)
+      words
+      (recur (gen-title-words chain)))))
 
 (defn gen-title
   ([] (gen-title markov-chain))
   ([chain]
-   (-> (s/join " "
-               (take 100   ; Make sure we eventually drop out
-                     (take-while (partial not= "🔚")
-                                 (drop-while #(or (= "🔚" %) (re-matches #"(\p{Punct})+" %))   ; Drop leading title breaks and punctuation
-                                             (mc/generate chain)))))
-       (s/replace #"\s+([!?:;,\"…\*\.])" "$1")   ; Collapse whitespace before punctuation
-       (s/replace #"\s+'\s*s\s+"         "'s ")  ; Collapse orphaned plurals
-       (s/replace #"\s+'\s*S\s+"         "'S ")  ;    "         "      "
-       (s/replace #"\s+\(\s+"            " (")   ; Collapse orphaned parens
-       (s/replace #"\s+\)\s+"            ") ")   ;    "         "      "
-       )))
+   (-> (s/join " " (gen-minimum-length-title chain))
+       (s/replace #"\s+([!?:;,\"…\*\.])" "$1")     ; Collapse whitespace before punctuation
+       (s/replace #"\s+'\s*s\s+"         "'s ")    ; Collapse orphaned plurals
+       (s/replace #"\s+'\s*S\s+"         "'S ")    ;    "         "      "
+       (s/replace #"\s+\(\s+"            " (")     ; Collapse orphaned parens
+       (s/replace #"\s+\)\s+"            ") "))))  ;    "         "      "
