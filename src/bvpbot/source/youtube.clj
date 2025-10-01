d831e7032f8a42318521299372d5c3d707815a7d
Peter Monks <pmonks@gmail.com>
2025-09-30 18:33:29 -0700

    :art: Remove old commented out code

--- src/bvpbot/source/youtube.clj
+++ src/bvpbot/source/youtube.clj
@@ -17,8 +17,6 @@
 
 (def ^:private api-host                    "https://www.googleapis.com")
 (def ^:private endpoint-get-channel-info   "/youtube/v3/channels?part=snippet&id=%s")
-;(def ^:private endpoint-get-channel-videos "/youtube/v3/search?part=snippet&order=date&type=video&maxResults=50&channelId=%s")
-
 (def ^:private endpoint-get-playlist-items "/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=%s")
 
 (defn- google-api-call
@@ -36,45 +34,6 @@
   "Returns info for the given channel, or throws an ex-info."
   [youtube-api-token channel-id]
   (:snippet (first (:items (google-api-call youtube-api-token (format endpoint-get-channel-info channel-id))))))
-
-;####TEST!!!!
-(comment
-(defn- sanitise-video-data-structure
-  "Sanitise (as in 'make sane'...) the herpaderp YouTube API video data structure. 🙄"
-  [items]
-  (seq (keep identity (map #(assoc (:snippet %) :id (:video-id (:id %))) items))))
-
-(defn- remove-premieres
-  "Removes 'premieres' (empty placeholders) from the given sequence of YouTube videos, because the herpaderp YouTube APIs don't support this directly. 🙄"   ; See https://stackoverflow.com/a/66596615/369849
-  [videos]
-  (remove #(= "upcoming" (:live-broadcast-content %)) videos))
-
-(defn videos
-  "Retrieves up to 50 videos for the given YouTube channel (or nil if there aren't any) newest first, optionally limited to those published since the given date."
-  ([youtube-api-token channel-id] (videos youtube-api-token nil channel-id))
-  ([youtube-api-token since channel-id]
-    (remove-premieres
-      (let [endpoint (if since
-                       (format (str endpoint-get-channel-videos "&publishedAfter=%s") channel-id (str (tm/instant since)))
-                       (format endpoint-get-channel-videos channel-id))]
-        (sanitise-video-data-structure (:items (google-api-call youtube-api-token endpoint)))))))
-
-(defn all-videos
-  "Retrieves all videos for the given YouTube channel, newest first.  Use with caution: this can be very expensive from a quota unit perspective (100 units / 50 videos)."
-  [youtube-api-token channel-id]
-  (remove-premieres
-    (loop [endpoint     (format endpoint-get-channel-videos channel-id)
-           publish-date (tm/instant)
-           result       []]
-      (let [items            (sanitise-video-data-structure (:items (google-api-call youtube-api-token endpoint)))
-            new-publish-date (:published-at (last items))]
-        (if (and (not (s/blank? new-publish-date))
-                 (tm/before? (tm/instant new-publish-date) (tm/instant publish-date)))
-          (recur (format (str endpoint-get-channel-videos "&publishedBefore=%s") channel-id new-publish-date)
-                 new-publish-date
-                 (into result items))
-          (into result items))))))
-)
 
 (defn channel-id->playlist-id
   "Turns a YouTube channel id into a playlist id for the default playlist in
